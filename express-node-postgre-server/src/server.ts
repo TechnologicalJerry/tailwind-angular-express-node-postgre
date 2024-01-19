4ad06abdfcca9faf097c643c269c6f17b35401b5
technologicaljerry <cserajatverma@gmail.com>
2024-01-19 22:56:56 +0530

    Update server.ts

--- express-node-postgre-server/src/server.ts
+++ express-node-postgre-server/src/server.ts
@@ -1,1 +1,5 @@
 import express, { Application } from "express";
+
+const app: Application = express();
+const server: Server = new Server(app);
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
