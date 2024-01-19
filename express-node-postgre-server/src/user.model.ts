d5cb028bdd0b99556273e67362b4ccdc7b05bb24
technologicaljerry <cserajatverma@gmail.com>
2024-01-19 22:49:50 +0530

    Update user.model.ts

--- express-node-postgre-server/src/user.model.ts
+++ express-node-postgre-server/src/user.model.ts
@@ -2,11 +2,11 @@ import { Model, Table, Column, DataType } from "sequelize-typescript";
 
 @Table({ tableName: "users" })
 export default class User extends Model {
-    @Column({
-        type: DataType.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        field: "id"
-      })
-      id?: number;
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: "id",
+  })
+  id?: number;
 }
