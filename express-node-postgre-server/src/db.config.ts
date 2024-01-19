05aec62f7f39f786461272014fa84191950a9290
technologicaljerry <cserajatverma@gmail.com>
2024-01-19 22:54:59 +0530

    Update db.config.ts

--- express-node-postgre-server/src/db.config.ts
+++ express-node-postgre-server/src/db.config.ts
@@ -1,3 +1,14 @@
-export const config = {};
-
-export const dialect = "postgres";
+export const config = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "root",
+    DB: "dbname",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+  
+  export const dialect = "postgres";
